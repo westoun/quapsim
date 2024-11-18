1719d028de5d56268dd1560bd806447925c3de3c
westoun <christoph.werner.stein@gmail.com>
2024-11-18 09:42:01 +0100

    Implement dict-based caching

--- quapsim/cache/simple_dict_cache.py
+++ quapsim/cache/simple_dict_cache.py
@@ -7,20 +7,32 @@ from .interface import ICache
 from quapsim.gates import IGate
 
 
+def create_key(gates: List[IGate]) -> str:
+    key = "_".join([gate.__repr__() for gate in gates])
+    return key
+
+
 class SimpleDictCache(ICache):
     """Base class for all gate sequence caches."""
 
+    def __init__(self):
+        self._dict = {}
+
     def add(self, gate_sequence: List[IGate], unitary: np.ndarray) -> None:
         """Add the unitary of a sequence of gates to the cache."""
-        raise NotImplementedError()
+        key = create_key(gate_sequence)
+        self._dict[key] = unitary
 
-    def retrieve(
-        self, gate_sequence: List[IGate], unitary: np.ndarray
-    ) -> Union[None, np.ndarray]:
+    def retrieve(self, gate_sequence: List[IGate]) -> Union[None, np.ndarray]:
         """Retrieve the unitary of a sequence of gates from the cache
         if it exists. Else return None."""
-        raise NotImplementedError()
+        key = create_key(gate_sequence)
+
+        if key not in self._dict:
+            return None
+        else:
+            return self._dict[key]
 
     def reset(self) -> None:
         """Clear the cache."""
-        raise NotImplementedError()
+        self._dict = {}
