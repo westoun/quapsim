daa3a2bb439ce02e83e2edc55f4e581e76f271e6
Christoph Stein <christophstein@Christophs-MacBook-Pro.local>
2025-03-25 16:04:12 +0100

    Add CT gate

--- quapsim/gates/__init__.py
+++ quapsim/gates/__init__.py
@@ -1,7 +1,7 @@
 from .interface import IGate
 from .swap import Swap
 from .single_qubit_gates import Gate, H, X, Y, Z, RX, RY, RZ, Phase, S, T, Identity
-from .controlled_gates import CGate, CX, CY, CZ, CRX, CRY, CRZ, CH, CS, CPhase
+from .controlled_gates import CGate, CX, CY, CZ, CRX, CRY, CRZ, CH, CS, CPhase, CT
 from .double_controlled_gates import CCGate, CCX, CCZ
 from .utils import (
     create_unitary,
