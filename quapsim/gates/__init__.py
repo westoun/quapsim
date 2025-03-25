d6068b60446f096cc5c29546316860422e877154
Christoph Stein <christophstein@dyn-eduroam218078.wlan.jku.at>
2025-03-25 09:48:51 +0100

    Add identity matrix

--- quapsim/gates/__init__.py
+++ quapsim/gates/__init__.py
@@ -1,6 +1,6 @@
 from .interface import IGate
 from .swap import Swap
-from .single_qubit_gates import Gate, H, X, Y, Z, RX, RY, RZ, Phase, S, T
+from .single_qubit_gates import Gate, H, X, Y, Z, RX, RY, RZ, Phase, S, T, Identity
 from .controlled_gates import CGate, CX, CY, CZ, CRX, CRY, CRZ, CH, CS, CPhase
 from .double_controlled_gates import CCGate, CCX, CCZ
 from .utils import (
