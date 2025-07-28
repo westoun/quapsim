72add4bfebe46440624e5e059ca35c6bd3a3c56f
Christoph Stein <christophstein@dyn-eduroam216036.wlan.jku.at>
2025-07-28 13:24:07 +0200

    Move evaluation code to separate folders

---
+++ evaluation/ga_case_study/extract_ga_results.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python3
+
+import json
+from os import listdir
+import re
+from typing import List, Dict
+
+
+def duration_to_seconds(duration: str) -> float:
+    hours = int(duration.split(":")[0])
+    minutes = int(duration.split(":")[1])
+    seconds = float(duration.split(":")[2])
+
+    return seconds + minutes * 60 + hours * 60 * 60
+
+
+log_file_folder = "results"
+
+log_file_paths = [
+    log_file_folder + "/" + path
+    for path in listdir(log_file_folder)
+    if path.startswith("experiment") and path.endswith(".log")
+]
+
+experiment_data: List[Dict] = []
+
+for log_file_path in log_file_paths:
+    experiment = {
+        "file_path": log_file_path,
+        "duration_in": "seconds",
+        "started_at": None,
+        "params": {
+            "cache_size": None,
+            "merging_rounds": None,
+            "seed": None,
+            "tag": None,
+            "population_size": None,
+            "generations": None,
+            "chromosome_length": None
+        },
+        "generations": []
+    }
+
+    new_generation = True
+    with open(log_file_path, "r") as log_file:
+        for i, line in enumerate(log_file):
+            if new_generation:
+                experiment["generations"].append({
+                    "i": len(experiment["generations"]),
+                    "population_redundancy": None,
+                    "build_gate_frequency_dict": {"duration": None},
+                    "optimize_gate_order": {
+                        "duration": None,
+                    },
+                    "generate_seed_bigrams": {
+                        "duration": None,
+                    },
+                    "consolidate_ngrams": {
+                        "duration": None,
+                    },
+                    "select_ngrams_to_cache": {"duration": None},
+                    "fill_cache": {"duration": None, },
+                    "simulate_using_cache": {
+                        "duration": None,
+                        "trie_lookup_duration": None,
+                    },
+                    "build_cache": {
+                        "duration": None
+                    },
+                    "simulate_without_cache": {"duration": None},
+                    "mean_fitness": None,
+                    "best_fitness": None,
+                    "best_circuit": None,
+                    "gate_type_dist": {},
+                    "cached_ngrams": None,
+                    "cache_hits_by_ngram": None
+                })
+                new_generation = False
+
+            experiment_setup = re.search(
+                r"cache_size=([0-9]+), merging_rounds=([0-9]+), seed=([0-9]+|None), tag=([a-zA-Z_\-]+|None), population_size=([0-9]+), generations=([0-9]+), chromosome_length=([0-9]+)",
+                line,
+            )
+            if experiment_setup is not None:
+                timestamp = " ".join(line.split(" ")[:2])
+
+                cache_size = experiment_setup.group(1)
+                merging_rounds = experiment_setup.group(2)
+                seed = experiment_setup.group(3)
+                tag = experiment_setup.group(4)
+                population_size = experiment_setup.group(5)
+                generations = experiment_setup.group(6)
+                chromosome_length = experiment_setup.group(7)
+
+                experiment["started_at"] = timestamp
+
+                experiment["params"]["cache_size"] = int(cache_size)
+                experiment["params"]["merging_rounds"] = int(merging_rounds)
+                experiment["params"]["population_size"] = int(population_size)
+                experiment["params"]["generations"] = int(generations)
+                experiment["params"]["chromosome_length"] = int(
+                    chromosome_length)
+
+                if seed != "None":
+                    experiment["params"]["seed"] = int(seed)
+
+                if tag != "None":
+                    experiment["params"]["tag"] = tag
+
+                continue
+
+            build_gate_frequency_dict_duration = re.search(
+                r"Executing _build_gate_frequency_dict took ([0-9\.\:]+).", line
+            )
+            if build_gate_frequency_dict_duration is not None:
+                experiment["generations"][-1]["build_gate_frequency_dict"]["duration"] = (
+                    duration_to_seconds(
+                        build_gate_frequency_dict_duration.group(1))
+                )
+                continue
+
+            optimize_gate_order_duration = re.search(
+                r"Executing _optimize_gate_order took ([0-9\.\:]+).", line
+            )
+            if optimize_gate_order_duration is not None:
+                experiment["generations"][-1]["optimize_gate_order"]["duration"] = duration_to_seconds(
+                    optimize_gate_order_duration.group(1)
+                )
+                continue
+
+            generate_seed_bigrams_duration = re.search(
+                r"Executing _generate_seed_bigrams took ([0-9\.\:]+).", line
+            )
+            if generate_seed_bigrams_duration is not None:
+                experiment["generations"][-1]["generate_seed_bigrams"]["duration"] = duration_to_seconds(
+                    generate_seed_bigrams_duration.group(1)
+                )
+                continue
+
+            consolidate_ngrams_duration = re.search(
+                r"Executing _consolidate_ngrams took ([0-9\.\:]+).", line
+            )
+            if consolidate_ngrams_duration is not None:
+                experiment["generations"][-1]["consolidate_ngrams"]["duration"] = duration_to_seconds(
+                    consolidate_ngrams_duration.group(1)
+                )
+                continue
+
+            select_ngrams_to_cache_duration = re.search(
+                r"Executing _select_ngrams_to_cache took ([0-9\.\:]+).", line
+            )
+            if select_ngrams_to_cache_duration is not None:
+                experiment["generations"][-1]["select_ngrams_to_cache"]["duration"] = duration_to_seconds(
+                    select_ngrams_to_cache_duration.group(1)
+                )
+                continue
+
+            fill_cache_duration = re.search(
+                r"Executing _fill_cache took ([0-9\.\:]+).", line
+            )
+            if fill_cache_duration is not None:
+                experiment["generations"][-1]["fill_cache"]["duration"] = duration_to_seconds(
+                    fill_cache_duration.group(1)
+                )
+                continue
+
+            simulate_without_cache_duration = re.search(
+                r"Executing simulate_without_cache took ([0-9\.\:]+).", line
+            )
+            if simulate_without_cache_duration is not None:
+                experiment["generations"][-1]["simulate_without_cache"]["duration"] = duration_to_seconds(
+                    simulate_without_cache_duration.group(1)
+                )
+                continue
+
+            simulate_using_cache_duration = re.search(
+                r"Executing simulate_using_cache took ([0-9\.\:]+).", line
+            )
+            if simulate_using_cache_duration is not None:
+                experiment["generations"][-1]["simulate_using_cache"]["duration"] = duration_to_seconds(
+                    simulate_using_cache_duration.group(1)
+                )
+                continue
+
+            build_cache_duration = re.search(
+                r"Executing build_cache took ([0-9\.\:]+)\.", line
+            )
+            if build_cache_duration is not None:
+                experiment["generations"][-1]["build_cache"]["duration"] = (
+                    duration_to_seconds(build_cache_duration.group(1))
+                )
+                continue
+
+            trie_lookup_duration = re.search(
+                r"Time during merging spent on trie lookup: ([0-9\.\:]+)", line
+            )
+            if trie_lookup_duration is not None:
+                experiment["generations"][-1]["simulate_using_cache"]["trie_lookup_duration"] = (
+                    duration_to_seconds(trie_lookup_duration.group(1))
+                )
+                continue
+
+            population_redundancy = re.search(
+                r"Population redundancy in generation [0-9]+: ([0-9]+\.[0-9]+)", line)
+            if population_redundancy is not None:
+                experiment["generations"][-1]["population_redundancy"] = float(
+                    population_redundancy.group(1)
+                )
+                continue
+
+            best_fitness = re.search(
+                r"Best fitness at generation [0-9]+: ([0-9]+\.[0-9]+)", line)
+            if best_fitness is not None:
+                experiment["generations"][-1]["best_fitness"] = float(
+                    best_fitness.group(1)
+                )
+                continue
+
+            mean_fitness = re.search(
+                r"Mean fitness at generation [0-9]+: ([0-9]+\.[0-9]+)", line)
+            if mean_fitness is not None:
+                experiment["generations"][-1]["mean_fitness"] = float(
+                    mean_fitness.group(1)
+                )
+                continue
+
+            best_circuit = re.search(
+                r"Best circuit at generation [0-9]+: (\[.+\])", line)
+            if best_circuit is not None:
+                experiment["generations"][-1]["best_circuit"] = best_circuit.group(
+                    1)
+                new_generation = True
+                continue
+
+            gate_type_distribution = re.search(
+                r"Distribution of gate types: (\{.+\})", line
+            )
+            if gate_type_distribution is not None:
+                type_distribution = json.loads(
+                    gate_type_distribution.group(1).replace("'", "\""))
+
+                experiment["generations"][-1]["gate_type_dist"] = type_distribution
+
+                continue
+
+            cached_ngrams = re.search(
+                r"Cached ngrams: (\[.+\])", line
+            )
+            if cached_ngrams is not None:
+                cached_ngrams_list = json.loads(
+                    cached_ngrams.group(1).replace("'", "\""))
+
+                experiment["generations"][-1]["cached_ngrams"] = cached_ngrams_list
+
+                continue
+
+            cache_hit_frequencies = re.search(
+                r"Cache hits by ngram: (\{.+\})", line)
+            if cache_hit_frequencies is not None:
+                cache_hits = json.loads(
+                    cache_hit_frequencies.group(1).replace("'", "\""))
+
+                experiment["generations"][-1]["cache_hits_by_ngram"] = cache_hits
+
+                continue
+
+    experiment_data.append(experiment)
+
+with open("experiment_ga_results.json", "w") as target_file:
+    json.dump(experiment_data, target_file)
---
+++ evaluation/gate_sim_experiments/utils.py
@@ -0,0 +1,413 @@
+#!/usr/bin/env python3
+
+import logging
+import numpy as np
+from random import choice, randint, sample, random, choices
+from typing import Tuple, List
+
+from quapsim import Circuit as QuapsimCircuit
+import quapsim.gates as QuapsimGates
+from quapsim.simulator import compute_redundancy
+
+from quasim import Circuit as QuasimCircuit
+import quasim.gates as QuasimGates
+
+SINGLE_GATES = [
+    "H",
+    "X",
+    "Y",
+    "Z",
+    "S",
+    "T",
+]
+SINGLE_PARAMETERIZED_GATES = [
+    "RX",
+    "RY",
+    "RZ",
+    "PHASE",
+]
+CONTROLLED_GATES = [
+    "SWAP",
+    "CS",
+    "CH",
+    "CX",
+    "CY",
+    "CZ",
+]
+CONTROLLED_PARAMETERIZED_GATES = [
+    "CRX",
+    "CRY",
+    "CRZ",
+    "CPhase",
+]
+DOUBLE_CONTROLLED_GATES = [
+    "CCX",
+    "CCZ",
+]
+
+# Specify how many distinct parameter values each
+# parameterized circuit can possibly take on.
+PARAMETER_AMOUNT: int = 200
+
+
+def create_random_gate_configs(
+    gate_count: int = 10,
+    qubit_num: int = 4,
+    entangle_first: bool = False,
+    uniform_configuration_choice: bool = True
+) -> List[Tuple]:
+    gate_configs: List[Tuple] = []
+
+    existing_gates: int = 0
+    if entangle_first:
+        gate_configs.append(("H", 0))
+        existing_gates += 1
+
+        for control_qubit in range(qubit_num - 1):
+            target_qubit = control_qubit + 1
+            gate_configs.append(("CX", control_qubit, target_qubit))
+            existing_gates += 1
+
+    assert (
+        existing_gates <= gate_count
+    ), "If you set entangle_first=True, you need to raise the desired gate count."
+
+    single_gate_type_count = len(SINGLE_GATES) * qubit_num
+    single_parameterized_gate_type_count = (
+        len(SINGLE_PARAMETERIZED_GATES) * PARAMETER_AMOUNT * qubit_num
+    )
+    controlled_gate_type_count = len(
+        CONTROLLED_GATES) * qubit_num * (qubit_num - 1)
+    controlled_parameterized_gate_type_count = (
+        len(CONTROLLED_PARAMETERIZED_GATES)
+        * PARAMETER_AMOUNT
+        * qubit_num
+        * (qubit_num - 1)
+    )
+    double_controlled_gate_type_count = (
+        len(DOUBLE_CONTROLLED_GATES) * qubit_num *
+        (qubit_num - 1) * (qubit_num - 2)
+    )
+
+    # The amount of unique gate configurations
+    total_gate_type_count = (
+        single_gate_type_count
+        + single_parameterized_gate_type_count
+        + controlled_gate_type_count
+        + controlled_parameterized_gate_type_count
+        + double_controlled_gate_type_count
+    )
+
+    # Avoid logging the total amount of gate configs more than once without having
+    # to pass around additional params.
+    if not hasattr(create_random_gate_configs, "_logged_total"):
+        logging.debug(
+            f"Considering a total amount of {total_gate_type_count} unique gate configurations."
+        )
+        create_random_gate_configs._logged_total = True
+
+    if uniform_configuration_choice:
+        # First pick a gate set based on the amount of unique
+        # gate types within the set in relation to the total amount
+        # of gate types. Then, within a set, each gate type occurrs
+        # equally often.
+        candidate_sets: List[List[str]] = choices(
+            population=[
+                SINGLE_GATES,
+                SINGLE_PARAMETERIZED_GATES,
+                CONTROLLED_GATES,
+                CONTROLLED_PARAMETERIZED_GATES,
+                DOUBLE_CONTROLLED_GATES,
+            ],
+            weights=[
+                single_gate_type_count / total_gate_type_count,
+                single_parameterized_gate_type_count / total_gate_type_count,
+                controlled_gate_type_count / total_gate_type_count,
+                controlled_parameterized_gate_type_count / total_gate_type_count,
+                double_controlled_gate_type_count / total_gate_type_count,
+            ],
+            k=gate_count - existing_gates,
+        )
+    else:
+        full_gate_set = SINGLE_GATES + SINGLE_PARAMETERIZED_GATES + \
+            CONTROLLED_GATES + CONTROLLED_PARAMETERIZED_GATES + DOUBLE_CONTROLLED_GATES
+        candidate_sets: List[List[str]] = [
+            full_gate_set
+            for _ in range(gate_count - existing_gates)
+        ]
+
+    for gate_set in candidate_sets:
+        gate_type = choice(gate_set)
+
+        if gate_type == "H":
+            target_qubit = randint(0, qubit_num - 1)
+            gate_configs.append((gate_type, target_qubit))
+
+        elif gate_type == "X":
+            target_qubit = randint(0, qubit_num - 1)
+            gate_configs.append((gate_type, target_qubit))
+
+        elif gate_type == "Y":
+            target_qubit = randint(0, qubit_num - 1)
+            gate_configs.append((gate_type, target_qubit))
+
+        elif gate_type == "Z":
+            target_qubit = randint(0, qubit_num - 1)
+            gate_configs.append((gate_type, target_qubit))
+
+        elif gate_type == "S":
+            target_qubit = randint(0, qubit_num - 1)
+            gate_configs.append((gate_type, target_qubit))
+
+        elif gate_type == "T":
+            target_qubit = randint(0, qubit_num - 1)
+            gate_configs.append((gate_type, target_qubit))
+
+        elif gate_type == "CS":
+            target_qubit, control_qubit = sample(range(0, qubit_num), 2)
+            gate_configs.append((gate_type, control_qubit, target_qubit))
+
+        elif gate_type == "CH":
+            target_qubit, control_qubit = sample(range(0, qubit_num), 2)
+            gate_configs.append((gate_type, control_qubit, target_qubit))
+
+        elif gate_type == "CX":
+            target_qubit, control_qubit = sample(range(0, qubit_num), 2)
+            gate_configs.append((gate_type, control_qubit, target_qubit))
+
+        elif gate_type == "CY":
+            target_qubit, control_qubit = sample(range(0, qubit_num), 2)
+            gate_configs.append((gate_type, control_qubit, target_qubit))
+
+        elif gate_type == "CZ":
+            target_qubit, control_qubit = sample(range(0, qubit_num), 2)
+            gate_configs.append((gate_type, control_qubit, target_qubit))
+
+        elif gate_type == "RX":
+            target_qubit = randint(0, qubit_num - 1)
+
+            theta = randint(0, PARAMETER_AMOUNT - 1) * \
+                (4 * np.pi) / (PARAMETER_AMOUNT)
+
+            gate_configs.append((gate_type, target_qubit, theta))
+
+        elif gate_type == "RY":
+            target_qubit = randint(0, qubit_num - 1)
+
+            theta = randint(0, PARAMETER_AMOUNT - 1) * \
+                (4 * np.pi) / (PARAMETER_AMOUNT)
+
+            gate_configs.append((gate_type, target_qubit, theta))
+
+        elif gate_type == "RZ":
+            target_qubit = randint(0, qubit_num - 1)
+
+            theta = randint(0, PARAMETER_AMOUNT - 1) * \
+                (4 * np.pi) / (PARAMETER_AMOUNT)
+
+            gate_configs.append((gate_type, target_qubit, theta))
+
+        elif gate_type == "CRX":
+            target_qubit, control_qubit = sample(range(0, qubit_num), 2)
+
+            theta = randint(0, PARAMETER_AMOUNT - 1) * \
+                (4 * np.pi) / (PARAMETER_AMOUNT)
+
+            gate_configs.append(
+                (gate_type, control_qubit, target_qubit, theta))
+
+        elif gate_type == "CRY":
+            target_qubit, control_qubit = sample(range(0, qubit_num), 2)
+
+            theta = randint(0, PARAMETER_AMOUNT - 1) * \
+                (4 * np.pi) / (PARAMETER_AMOUNT)
+
+            gate_configs.append(
+                (gate_type, control_qubit, target_qubit, theta))
+
+        elif gate_type == "CPhase":
+            target_qubit, control_qubit = sample(range(0, qubit_num), 2)
+
+            theta = randint(0, PARAMETER_AMOUNT - 1) * \
+                (4 * np.pi) / (PARAMETER_AMOUNT)
+
+            gate_configs.append(
+                (gate_type, control_qubit, target_qubit, theta))
+
+        elif gate_type == "CRZ":
+            target_qubit, control_qubit = sample(range(0, qubit_num), 2)
+
+            theta = randint(0, PARAMETER_AMOUNT - 1) * \
+                (4 * np.pi) / (PARAMETER_AMOUNT)
+
+            gate_configs.append(
+                (gate_type, control_qubit, target_qubit, theta))
+
+        elif gate_type == "CCX":
+            target_qubit, control_qubit1, control_qubit2 = sample(
+                range(0, qubit_num), 3
+            )
+
+            gate_configs.append(
+                (gate_type, control_qubit1, control_qubit2, target_qubit)
+            )
+
+        elif gate_type == "CCZ":
+            target_qubit, control_qubit1, control_qubit2 = sample(
+                range(0, qubit_num), 3
+            )
+
+            gate_configs.append(
+                (gate_type, control_qubit1, control_qubit2, target_qubit)
+            )
+
+        elif gate_type == "PHASE":
+            target_qubit = randint(0, qubit_num - 1)
+
+            theta = randint(0, PARAMETER_AMOUNT - 1) * \
+                (4 * np.pi) / (PARAMETER_AMOUNT)
+
+            gate_configs.append((gate_type, target_qubit, theta))
+
+        elif gate_type == "SWAP":
+            qubit1, qubit2 = sample(range(0, qubit_num), 2)
+
+            gate_configs.append((gate_type, qubit1, qubit2))
+
+        else:
+            raise NotImplementedError()
+
+    return gate_configs
+
+
+def build_quapsim_circuit(gate_configs: List[Tuple], qubit_num: int) -> QuapsimCircuit:
+    quapsim_circuit = QuapsimCircuit(qubit_num)
+
+    for gate_config in gate_configs:
+        gate_type = gate_config[0]
+
+        if gate_type == "H":
+            target_qubit = gate_config[1]
+            quapsim_circuit.apply(QuapsimGates.H(target_qubit))
+
+        elif gate_type == "X":
+            target_qubit = gate_config[1]
+            quapsim_circuit.apply(QuapsimGates.X(target_qubit))
+
+        elif gate_type == "Y":
+            target_qubit = gate_config[1]
+            quapsim_circuit.apply(QuapsimGates.Y(target_qubit))
+
+        elif gate_type == "Z":
+            target_qubit = gate_config[1]
+            quapsim_circuit.apply(QuapsimGates.Z(target_qubit))
+
+        elif gate_type == "S":
+            target_qubit = gate_config[1]
+            quapsim_circuit.apply(QuapsimGates.S(target_qubit))
+
+        elif gate_type == "T":
+            target_qubit = gate_config[1]
+            quapsim_circuit.apply(QuapsimGates.T(target_qubit))
+
+        elif gate_type == "CS":
+            control_qubit, target_qubit = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.CS(control_qubit, target_qubit))
+
+        elif gate_type == "CH":
+            control_qubit, target_qubit = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.CH(control_qubit, target_qubit))
+
+        elif gate_type == "CX":
+            control_qubit, target_qubit = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.CX(control_qubit, target_qubit))
+
+        elif gate_type == "CY":
+            control_qubit, target_qubit = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.CY(control_qubit, target_qubit))
+
+        elif gate_type == "CZ":
+            control_qubit, target_qubit = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.CZ(control_qubit, target_qubit))
+
+        elif gate_type == "RX":
+            target_qubit, theta = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.RX(target_qubit, theta))
+
+        elif gate_type == "RY":
+            target_qubit, theta = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.RY(target_qubit, theta))
+
+        elif gate_type == "RZ":
+            target_qubit, theta = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.RZ(target_qubit, theta))
+
+        elif gate_type == "CRX":
+            control_qubit, target_qubit, theta = (
+                gate_config[1],
+                gate_config[2],
+                gate_config[3],
+            )
+            quapsim_circuit.apply(QuapsimGates.CRX(
+                control_qubit, target_qubit, theta))
+
+        elif gate_type == "CRY":
+            control_qubit, target_qubit, theta = (
+                gate_config[1],
+                gate_config[2],
+                gate_config[3],
+            )
+            quapsim_circuit.apply(QuapsimGates.CRY(
+                control_qubit, target_qubit, theta))
+
+        elif gate_type == "CPhase":
+            control_qubit, target_qubit, theta = (
+                gate_config[1],
+                gate_config[2],
+                gate_config[3],
+            )
+            quapsim_circuit.apply(
+                QuapsimGates.CPhase(control_qubit, target_qubit, theta)
+            )
+
+        elif gate_type == "CRZ":
+            control_qubit, target_qubit, theta = (
+                gate_config[1],
+                gate_config[2],
+                gate_config[3],
+            )
+            quapsim_circuit.apply(QuapsimGates.CRZ(
+                control_qubit, target_qubit, theta))
+
+        elif gate_type == "CCX":
+            control_qubit1, control_qubit2, target_qubit = (
+                gate_config[1],
+                gate_config[2],
+                gate_config[3],
+            )
+            quapsim_circuit.apply(
+                QuapsimGates.CCX(control_qubit1, control_qubit2, target_qubit)
+            )
+
+        elif gate_type == "CCZ":
+            control_qubit1, control_qubit2, target_qubit = (
+                gate_config[1],
+                gate_config[2],
+                gate_config[3],
+            )
+            quapsim_circuit.apply(
+                QuapsimGates.CCZ(control_qubit1, control_qubit2, target_qubit)
+            )
+
+        elif gate_type == "PHASE":
+            target_qubit, theta = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.Phase(target_qubit, theta))
+
+        elif gate_type == "SWAP":
+            qubit1, qubit2 = gate_config[1], gate_config[2]
+            quapsim_circuit.apply(QuapsimGates.Swap(qubit1, qubit2))
+
+        else:
+            raise NotImplementedError()
+
+    return quapsim_circuit
