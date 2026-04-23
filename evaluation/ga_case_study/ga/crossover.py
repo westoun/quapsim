8cf7da94a1838f32330ae060123ed0b98ae0b76d
Christoph Stein <christoph.werner.stein@gmail.com>
2026-04-23 13:25:22 +0200

    Implement two point crossover

--- evaluation/ga_case_study/ga/crossover.py
+++ evaluation/ga_case_study/ga/crossover.py
@@ -1,10 +1,25 @@
 
 from quapsim import Circuit
+from copy import deepcopy
+from random import randint, sample
+from typing import Tuple
+
 
 class TwoPointCrossover():
 
     def __init__(self):
         pass
 
-    def cross(self, circuit1: Circuit, circuit2: Circuit):
-        pass 
+    def cross(self, circuit1: Circuit, circuit2: Circuit) -> Tuple[Circuit, Circuit]:
+        max_position = min(len(circuit1.gates), len(circuit2.gates))
+
+        crx_i1, crx_i2 = sample(range(max_position), k=2)
+        if crx_i1 > crx_i2:
+            crx_i1, crx_i2 = crx_i2, crx_i1
+
+        circuit1.gates[crx_i1:crx_i2], circuit2.gates[crx_i1:crx_i2] = (
+            deepcopy(circuit2.gates[crx_i1:crx_i2]),
+            deepcopy(circuit1.gates[crx_i1:crx_i2]),
+        )
+
+        return circuit1, circuit2
