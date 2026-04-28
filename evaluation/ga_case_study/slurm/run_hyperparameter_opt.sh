bf52a1c0c096b942d27b8dd45eccba304f7a0696
Christoph Stein <christoph.werner.stein@gmail.com>
2026-04-28 14:20:40 +0200

    Fix relative file paths

--- evaluation/ga_case_study/slurm/run_hyperparameter_opt.sh
+++ evaluation/ga_case_study/slurm/run_hyperparameter_opt.sh
@@ -4,16 +4,17 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --time=10-00:00:00
+#SBATCH --mem=1G
 
 echo "Activate Python and virtualenv"
-cd /home/ws/ws16/quapsim/
+cd /home/ws/ws16/quapsim/evaluation/ga_case_study
 
 module load python/3.11.1
 
 echo "Start GA Hyperparameter Opt"
 
 # Activate virtual environment
-source venv/bin/activate
+source /home/ws/ws16/quapsim/venv/bin/activate
 
 # Run the Python script
 echo python run_hyperparameter_opt.py -qn ${qubit_num} -gc ${gate_count} -ss ${selection_strategy} -st ${synthesis_target} -sc ${seed_count}
