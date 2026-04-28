ef6500f2c8658e8d12ba8d77fa44b8144679d22c
Christoph Stein <christoph.werner.stein@gmail.com>
2026-04-28 13:32:26 +0200

    Change max generations count

--- evaluation/ga_case_study/run_ga_experiment.py
+++ evaluation/ga_case_study/run_ga_experiment.py
@@ -162,7 +162,7 @@ def run_experiment(
         population_size=5000,
         mutation_prob=mutation_prob,
         crossover_prob=crossover_prob,
-        max_generations=20,
+        max_generations=5_000,
         simulator_params=simulator_params,
         cache_rebuild_frequency=rebuild_frequency,
         target_unitary=target_unitary,
