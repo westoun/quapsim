2ca3033114c0cee0101b7e2415052fc95ccfb0bf
Christoph Stein <christoph.werner.stein@gmail.com>
2026-04-28 14:22:47 +0200

    Fix broken directory paths

--- evaluation/ga_case_study/slurm/run_experiment.sh
+++ evaluation/ga_case_study/slurm/run_experiment.sh
@@ -6,14 +6,14 @@
 #SBATCH --time=10-00:00:00
 
 echo "Activate Python and virtualenv"
-cd /home/ws/ws16/quapsim/
+cd /home/ws/ws16/quapsim/evaluation/ga_case_study
 
 module load python/3.11.1
 
 echo "Start GA Experiment Run"
 
 # Activate virtual environment
-source venv/bin/activate
+source /home/ws/ws16/quapsim/venv/bin/activate
 
 # Run the Python script
 echo python run_ga_experiment.py -cs ${cache_size} -mr ${merging_rounds} -rf ${rebuild_frequency} -qn ${qubit_num} -gc ${gate_count} -ss ${selection_strategy} -st ${synthesis_target} -cp ${crossover_prob} -mp ${mutation_prob} -s ${seed} -t ${tag}
